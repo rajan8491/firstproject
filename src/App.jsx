9709424ed97da28d01de79f101ee729f28ebd36c
Rajan Yadav <rajankumaryadav409@gmail.com>
2025-03-28 12:15:43 +0530

    Adding Website Title

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,7 @@ function App() {
   return (
     <>
       <div className='w-full mx-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
+        <h1>Rajan's Password Generator</h1>
         <h1 className='text-white text-center'>Password generator</h1>
         <div className='flex shadow-md rounded-lg overflow-hidden mb-4'>
           <input 
